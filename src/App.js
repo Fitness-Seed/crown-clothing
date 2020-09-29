7e5a885ba1ef8f3509e88093fbabcb1a73201808
Sinovuyo <ngolomisinovuyo@gmail.com>
2020-09-29 18:36:35 +0200

    added user selector and cart selector

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import {createStructuredSelector} from 'reselect';
+import { selectCurrentUser } from './redux/user/user.selectors';
 import { setCurrentUser } from "./redux/user/user.actions";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
@@ -48,8 +50,8 @@ class App extends React.Component {
     );
   }
 }
-const mapStateToProps = ({ user }) => ({
-  currentUser: user,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
